2eb87c43c0c88fc258b5cf58cc2575cfc2798d22
FerdyRod <ferdyrodriguez@gmail.com>
2025-08-07 11:15:57 +0200

    add checksum to AllfundsNetworking binaryTarget in Swift Package

--- swift-package/Package.swift
+++ swift-package/Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AllfundsNetworking",
-            url: "https://github.com/ferdyrod/networking-sample/releases/download/0.0.01/networking-0.0.01.zip"
-            path: "networking.xcframework"
+            url: "https://github.com/ferdyrod/networking-sample/releases/download/0.0.01/networking-0.0.01.zip",
+            checksum: "d7b34d43f03f5f020f427798d5c7415a2b92e126f4ffc56537ed211011449954"
         ),
     ]
 )
