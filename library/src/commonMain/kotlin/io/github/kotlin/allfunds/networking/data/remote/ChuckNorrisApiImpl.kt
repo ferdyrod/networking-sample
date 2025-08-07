040a10c930cacc834a2f3d75b2fd280dcace7a07
FerdyRod <ferdyrodriguez@gmail.com>
2025-08-07 13:52:28 +0200

    Revert "add networking Swift Package definition, XCFramework, and integration instructions"
    This reverts commit a48403a2ba1365f3ba5187d8ea6bc2fdecfb1364.

--- library/src/commonMain/kotlin/io/github/kotlin/allfunds/networking/data/remote/ChuckNorrisApiImpl.kt
+++ library/src/commonMain/kotlin/io/github/kotlin/allfunds/networking/data/remote/ChuckNorrisApiImpl.kt
@@ -5,11 +5,8 @@ import io.github.kotlin.allfunds.networking.data.remote.dto.SearchResponseDto
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.plugins.*
 import io.ktor.client.request.*
 import io.ktor.serialization.kotlinx.json.*
-import io.ktor.utils.io.errors.*
-import kotlin.Throws
 import kotlinx.serialization.json.Json
 
 /**
@@ -30,11 +27,7 @@ class ChuckNorrisApiImpl : ChuckNorrisApi {
     
     /**
      * Get a random joke
-     * @throws ClientRequestException if the request fails
-     * @throws IOException if there's an I/O error
-     * @throws Exception for any other error
      */
-    @Throws(ClientRequestException::class, IOException::class, Exception::class)
     override suspend fun getRandomJoke(): JokeDto {
         return client.get("$baseUrl/random").body()
     }
@@ -42,11 +35,7 @@ class ChuckNorrisApiImpl : ChuckNorrisApi {
     /**
      * Get a random joke from a specific category
      * @param category The category to get a joke from
-     * @throws ClientRequestException if the request fails
-     * @throws IOException if there's an I/O error
-     * @throws Exception for any other error
      */
-    @Throws(ClientRequestException::class, IOException::class, Exception::class)
     override suspend fun getRandomJokeByCategory(category: String): JokeDto {
         return client.get("$baseUrl/random") {
             parameter("category", category)
@@ -55,11 +44,7 @@ class ChuckNorrisApiImpl : ChuckNorrisApi {
     
     /**
      * Get all available categories
-     * @throws ClientRequestException if the request fails
-     * @throws IOException if there's an I/O error
-     * @throws Exception for any other error
      */
-    @Throws(ClientRequestException::class, IOException::class, Exception::class)
     override suspend fun getCategories(): List<String> {
         return client.get("$baseUrl/categories").body()
     }
@@ -67,11 +52,7 @@ class ChuckNorrisApiImpl : ChuckNorrisApi {
     /**
      * Search for jokes
      * @param query The search query
-     * @throws ClientRequestException if the request fails
-     * @throws IOException if there's an I/O error
-     * @throws Exception for any other error
      */
-    @Throws(ClientRequestException::class, IOException::class, Exception::class)
     override suspend fun searchJokes(query: String): SearchResponseDto {
         return client.get("$baseUrl/search") {
             parameter("query", query)
--- library/src/commonMain/kotlin/io/github/kotlin/allfunds/networking/di/KoinInitializer.kt
+++ library/src/commonMain/kotlin/io/github/kotlin/allfunds/networking/di/KoinInitializer.kt
@@ -3,7 +3,6 @@ package io.github.kotlin.allfunds.networking.di
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.core.KoinApplication
-import kotlin.Throws
 
 /**
  * Initializer for Koin dependency injection
@@ -14,28 +13,18 @@ object KoinInitializer {
     /**
      * Initialize Koin with the network module
      * @return KoinApplication instance
-     * @throws IllegalStateException if Koin initialization fails
      */
-    @Throws(IllegalStateException::class)
     fun init(): KoinApplication {
-        try {
-            if (initialized) {
-                stopKoin()
-            }
-            
-            val koinApp = startKoin {
-                modules(networkModule)
-            }
-            
-            initialized = true
-            return koinApp
-        } catch (e: Exception) {
-            // Wrap any exception in IllegalStateException to ensure consistent error handling in Swift
-            if (e !is IllegalStateException) {
-                throw IllegalStateException("Failed to initialize Koin", e)
-            }
-            throw e
+        if (initialized) {
+            stopKoin()
         }
+        
+        val koinApp = startKoin {
+            modules(networkModule)
+        }
+        
+        initialized = true
+        return koinApp
     }
     
     /**
